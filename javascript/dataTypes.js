c2ed7d04dca59c355bc9d390eaba560d64806d0e
Thabung <thabungm@gmail.com>
2022-08-28 23:28:07 +0530

    added js intro

---
+++ javascript/dataTypes.js
@@ -0,0 +1,37 @@
+/* // string
+var courseName = 'MERN stack development';
+console.log(courseName);
+
+// Number datatype
+var century = 100;
+console.log(century);
+
+// boolean
+var isHelpful = true; //false
+console.log(isHelpful);
+
+// null
+var emptyBox = null;
+console.log(emptyBox);
+
+// undefined
+var earthIn2050;
+console.log(earthIn2050);
+
+var nameOfPerson = 'Mike'; */
+/*
+person
+    name: string
+    age: Number 
+    gender: Male/Female (string)
+    canRead: Yes/No Boolean
+    attendance:[true, false, true, true]
+*/
+// non-primitive data type
+var person = {
+  name: 'Mike',
+  age: 23,
+  gender: 'Male',
+  courseCompleted: false,
+};
+console.log(person);
