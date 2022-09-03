4c31c9ef90b22309fa2f0768d5a44aba31c4a742
Thabung <thabungm@gmail.com>
2022-09-03 15:34:57 +0530

    Adding functions and datatypes

--- javascript/dataTypes.js
+++ javascript/dataTypes.js
@@ -1,37 +1,54 @@
-/* // string
+/* string
 var courseName = 'MERN stack development';
-console.log(courseName);
+console.log(courseName); */
 
 // Number datatype
-var century = 100;
-console.log(century);
+// var century = 100;
+// console.log(century);
 
-// boolean
-var isHelpful = true; //false
+// // boolean
+/* var isHelpful = true; //false
 console.log(isHelpful);
 
 // null
-var emptyBox = null;
-console.log(emptyBox);
+// var emptyBox = null;
+// console.log(emptyBox);
 
-// undefined
-var earthIn2050;
-console.log(earthIn2050);
+// // undefined
+// var earthIn2050;
+// console.log(earthIn2050);
 
-var nameOfPerson = 'Mike'; */
-/*
+// var nameOfPerson = 'Mike';
 person
-    name: string
-    age: Number 
-    gender: Male/Female (string)
-    canRead: Yes/No Boolean
-    attendance:[true, false, true, true]
+name: string
+age: Number 
+gender: Male/Female (string)
+canRead: Yes/No Boolean
+attendance:[true, false, true, true]
+
 */
 // non-primitive data type
-var person = {
+/* var student = {
+  // key-value pair
   name: 'Mike',
   age: 23,
   gender: 'Male',
   courseCompleted: false,
+  address: {
+    contactNumber: 9866544,
+    mail: 'abc@abc.com',
+  },
 };
-console.log(person);
+
+const studentName = student.name;
+console.log(studentName);
+const contactNumber = student.address.contactNumber;
+console.log(contactNumber); */
+var bigNumber = Number.MAX_SAFE_INTEGER;
+// Big Integer / BigInt
+var bigInteger = BigInt(Number.MAX_SAFE_INTEGER);
+var bigInt100 = BigInt(100); // 100n
+
+console.log(bigInteger);
+console.log(bigInt100);
+console.log(bigInteger + bigInt100);
---
+++ javascript/functions/introduction.js
@@ -0,0 +1,31 @@
+// area of rectangle
+// length X breadth
+
+// 3 X 2
+/* var length1 = 3;
+var breadth1 = 2;
+console.log(length1 * breadth1);
+
+// 4 X 3
+var length2 = 4;
+var breadth2 = 3;
+console.log(length2 * breadth2);
+
+// 10 X 5
+var length3 = 10;
+var breadth3 = 5;
+console.log(length3 * breadth3); */
+// defining the function // building a machine
+function calculateArea(length, breadth) {
+  var area = length * breadth;
+  return area;
+}
+var result = calculateArea(5, 3);
+console.log(result);
+
+/*
+- Arrange the tea, milk, water in containers
+- boil the water
+- Mix the tea, milk, water
+- The tea is ready!!
+*/
