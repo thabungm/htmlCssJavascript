8fdf483f473dc15724219cbd3081d99fe785711e
Thabung <thabungm@gmail.com>
2022-09-10 22:21:20 +0530

    progress on objects

--- javascript/objects/workingWithObjects.js
+++ javascript/objects/workingWithObjects.js
@@ -1,8 +1,3 @@
-const course = {
-  name: 'MERN',
-  durationInMonths: 3,
-};
-
 const player = {
   name: 'Mike',
   score: 3,
@@ -24,9 +19,32 @@ console.log(player); */
 /* const player2 = Object.assign({}, player);
 player2.name = 'Xyz';
 console.log(player); */
-/* const player2 = { ...player };
-player2.name = 'Xyz';
-console.log(player) */
+/* const player2 = { ...player }; // shallow copy
+player2.name = 'Xyz'; */
+// player2.address.street = 'New street';
+// console.log(player);
+
+// deepCopy
+/* for (let i in player2) {
+  console.log(i);
+  console.log(player2[i]);
+} */
+
+/* function deepCopy(object) {
+  let finalCopy = {};
+
+  for (let i in object) {
+    if (typeof object[i] === 'object') {
+      finalCopy[i] = { ...deepCopy(object[i]) };
+    } else {
+      finalCopy[i] = object[i];
+    }
+  }
+  return finalCopy;
+}
+const player3 = deepCopy(player);
+player3.address.street = '3rd address';
+console.log(player); */
 
 /* 
 const mobile = player.address.contact.mobile;
@@ -35,7 +53,7 @@ const score = player.score;
  */
 
 // destructuring assignment
-const {
+/* const {
   name,
   score,
   address: {
@@ -43,4 +61,74 @@ const {
   },
 } = player;
 console.log(name, score, mobile);
+
+function recursiveFunction(evenNumber, maxCount) {
+  console.log(evenNumber);
+  if (evenNumber < maxCount - 1) {
+    recursiveFunction(evenNumber + 2, maxCount);
+  }
+}
+recursiveFunction(0, 9); */
+
+// recursiveFunction(0, 10);
+
 // get
+
+// detecting presence of property
+/* const course = {
+  name: 'MERN',
+  durationInMonths: 3,
+};
+course.name = 'HTML'; */
+// in finds the property across user defined and prototype properties
+// console.log('toString' in course);
+// console.log('boolean' in course);
+/* 
+console.log(course.hasOwnProperty('toString'));
+console.log(course.hasOwnProperty('name'));
+ */
+// console.log(Object.keys(player));
+// console.log(Object.values(player));
+// console.log(Object.entries(player));
+
+//property descriptors
+/* const property = Object.getOwnPropertyDescriptor(course, 'name');
+console.log(property);
+for (let i in course) {
+  console.log(i);
+} */
+
+// console.log(Object.getOwnPropertyDescriptor(course, 'name'));
+/* Object.defineProperty(course, 'name', {
+  value: player.address,
+  writable: true,
+  enumerable: true,
+  configurable: true,
+});
+console.log(course); */
+/* console.log(Object.getOwnPropertyDescriptor(course, 'name'));
+for (let i in course) {
+  console.log(i);
+}
+course.name = 'React';
+// delete course.name
+console.log(course); */
+
+// Object seal and object freeze
+const course = {
+  name: 'MERN',
+  durationInMonths: 3,
+};
+// course.isCompleted = false;
+// console.log(course);
+/* Object.seal(course);
+course.isCompleted = false;
+course.durationInMonths = 6;
+delete course.durationInMonths;
+console.log(course); */
+
+Object.freeze(course);
+course.name = 'Javascript';
+delete course.durationInMonths;
+
+console.log(course);
