0d37f84b0a08873b8f92e05de7d414abcae87554
Thabung <thabungm@gmail.com>
2022-10-08 22:04:46 +0530

    added changes for higher order function

--- javascript/functions/higherOrderFn/demo/src/js/index.js
+++ javascript/functions/higherOrderFn/demo/src/js/index.js
@@ -3,24 +3,34 @@ import currencyRates from './currencyRates.json';
 import { renderRows, renderTotal, renderCurrencySelector } from './view';
 
 const higherOrderFn = (currencyRate, fn) => {
-  function modifiedFn() {
-    const modifiedCartItems = cartItems.map(function (item) {
-      item['cost'] = item['cost'] * currencyRate;
-      return item;
+  function modifiedFn(inputCartItems) {
+    const modifiedCartItems = inputCartItems.map(function (item) {
+      return {
+        ...item,
+        cost: item.cost * currencyRate,
+      };
     });
     fn(modifiedCartItems);
   }
   return modifiedFn;
 };
 
-const newFn = higherOrderFn(72, renderTotal);
-newFn();
-const newFn2 = higherOrderFn(72, renderRows);
-newFn2();
+renderRows(cartItems);
+renderTotal(cartItems);
+renderCurrencySelector(currencyRates);
+
+const currencyPicker = document.querySelector('select[name="currency-picker"]');
+
+currencyPicker.addEventListener('change', (event) => {
+  higherOrderFn(event.target.value, renderRows)(cartItems);
+  higherOrderFn(event.target.value, renderTotal)(cartItems);
+});
+
 /* renderRows(cartItems);
 renderTotal(cartItems);
-renderCurrencySelector(currencyRates); */
+renderCurrencySelector(currencyRates); 
 // Model -> files which deal with the data
 // View -> rendering the UI
 // Controller -> responsible for control of flow
 // services -> has bisuness logic
+*/
