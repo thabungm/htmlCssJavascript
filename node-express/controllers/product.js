b8479bdb72738dff69b750cdafefa66007dba217
Thabung <thabungm@gmail.com>
2023-01-15 21:47:27 +0530

    Pagination

--- node-express/controllers/product.js
+++ node-express/controllers/product.js
@@ -6,8 +6,31 @@ export const create = async (data) => {
   return product;
 };
 
-export const get = async () => {
-  const result = await Product.find();
+export const get = async (params) => {
+  /*
+      {
+        keyword: orange,
+        sortBy: name,
+        sortDir: desc/asc,
+        filterBy: Active
+      }
+      */
+  const { keyword, sortBy, sortDir, perPage, pageNumber } = params;
+
+  // const keyword = params.keyword;
+
+  // total: 100 records
+  // per page: 5
+  // current page number 2, 3
+  // how many records skipped? : perPage * (currentPageNumber - 1)
+  const skip = perPage * (pageNumber - 1);
+  console.log({ skip, perPage });
+  const search = new RegExp(keyword, 'i');
+  const result = await Product.find({ name: search })
+    .sort({ [sortBy]: sortDir }) // sortBy: 'name', sortDir: -1
+    .skip(skip)
+    .limit(perPage);
+
   return result;
 };
 
--- node-express/routes/productRoutes.js
+++ node-express/routes/productRoutes.js
@@ -18,7 +18,32 @@ const productRoutes = (router) => {
 
   router.get('/products', async (req, res) => {
     try {
-      const data = await get(req.body);
+      // search: keyword | sort:fields,dir  | filter: field | pagination: page number
+      // mostly 4 common ways to send the data:
+      /*
+      - body
+      - query params
+      - query string
+      - Headers
+      */
+
+      /*
+      {
+        keyword: orange,
+        sortBy: name,
+        sortDir: desc/asc,
+        filterBy: Active
+      }
+      */
+      const data = await get(req.query);
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(400).json(error.message);
+    }
+  });
+  router.get('/products/:productId', async (req, res) => {
+    try {
+      const data = await get(req.params.productId);
       res.status(200).json(data);
     } catch (error) {
       res.status(400).json(error.message);
@@ -30,7 +55,7 @@ const productRoutes = (router) => {
     res.status(200).json('Updated');
   });
 
-  router.put('/products/:productId', async (req, res) => {
+  router.delete('/products/:productId', async (req, res) => {
     await remove(req.params.productId);
     res.status(200).json('Deleted');
   });
